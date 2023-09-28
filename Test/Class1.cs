c7b8352564f49e41d939b8c47dbb006abc134c3a
marta01h <isip_m.a.sargsyan@mpt.ru>
2023-09-28 21:16:27 +0300

    88888

---
+++ Test/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+
+namespace ClassLibrary1
+{
+    public class DataSerializer
+    {
+        public static string Serialize<T>(T data)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+
+namespace Test
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void SerializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var data = new { Name = "John", Age = 30 };
+            string serializedData = DataSerializer.Serialize(data);
+
+            // Удалите фигурные скобки из JSON-строки
+            serializedData = serializedData.TrimStart('{').TrimEnd('}');
+
+            ResultTextBlock.Text = $"Serialized Data: {serializedData}";
+        }
+
+
+        private void DeserializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            string serializedData = "{'Name':'Alice','Age':25}";
+            var deserializedData = DataSerializer.Deserialize<dynamic>(serializedData);
+            ResultTextBlock.Text = $"Deserialized Data: Name: {deserializedData.Name}, Age: {deserializedData.Age}";
+        }
+    }
+}
